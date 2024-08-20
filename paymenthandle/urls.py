d1528f44f9b4ed76cf82a19426c74edffdd5b8cc
sohayab1 <158809187+sohayab1@users.noreply.github.com>
2024-08-20 23:18:23 +0600

    First commit

--- paymenthandle/urls.py
+++ paymenthandle/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('payment/', views.process_payment, name='process_payment'),
     
-   path('payment/success/<str:name>/<int:num_rooms>/<str:room_type>/<str:payment_option>/<amount>/', views.payment_success, name='payment_success'),
+    path('payment/success/<str:name>/<int:num_rooms>/<str:room_type>/<str:payment_option>/<amount>/', views.payment_success, name='payment_success'),
 
     path('generate_pdf/<str:name>/<int:num_rooms>/<str:room_type>/<str:payment_option>/<amount>/', generate_pdf, name='generate_pdf'),
     path('get_hotel_name/', views.get_hotel_name, name='get_hotel_name'),
